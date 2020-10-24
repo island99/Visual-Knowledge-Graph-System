64e99fd19eb3f233195ee3b64776e49072c25485
island99 <18048381+island99@users.noreply.github.com>
2020-10-24 13:45:26 +0800

    Update all.js

--- backend/all.js
+++ backend/all.js
@@ -1,11 +1,10 @@
-var username = 'neo4j';
-var password = '000000';
+var config = require('./config.json')
 
 function allnodesPromise(query) {
     return new Promise((resolve, reject) => {
         // console.log(resolve);
         var neo4j = require('node-neo4j');
-        db = new neo4j(`http://${username}:${password}@localhost:7474`);
+        db = new neo4j(`http://${config.username}:${config.password}@localhost:7474`);
         db.cypherQuery(query, function(err, result) {
             if (err) {
                 reject(err);
@@ -16,4 +15,4 @@ function allnodesPromise(query) {
     });
 }
 module.exports.allnodesPromise = allnodesPromise
-    // module.exports.allnodes=allnodes	localhost:7687
+
